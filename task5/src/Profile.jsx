f292d56933c8e54c203eef739025ce0bb70ad86b
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-04 17:31:36 +0300

    profile

--- task5/src/Profile.jsx
+++ task5/src/Profile.jsx
@@ -7,14 +7,16 @@ const formatDate = (date) => moment(date).format('DD MMM YYYY');
 const Profile = (userData) => {
   return (
     <>
-      <div className="profile">
-        {' '}
-        <div className="profile__name">
-          {`${userData.firstName} ${userData.lastName}`}
+      <div>
+        <div className="profile">
+          {' '}
+          <div className="profile__name">
+            {`${userData.firstName} ${userData.lastName}`}
+          </div>
+          <div className="profile__birth">{`Was born ${formatDate(
+            userData.birthDate
+          )} in ${userData.birthPlace}`}</div>
         </div>
-        <div className="profile__birth">{`Was born ${formatDate(
-          userData.birthDate
-        )} in ${userData.birthPlace}`}</div>
       </div>
     </>
   );

4e7e19b6bf6008d61b2f5fd40d3f78a34ef4fdd8
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-05 10:58:59 +0300

    profile

--- task5/src/App.jsx
+++ task5/src/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Profile from './Profile';
 
-const userData = {
-  firstName: 'James',
-  lastName: 'Bond',
-  birthDate: '1991-01-17T11:11:11.819Z',
-  birthPlace: 'London',
-};
-
 const App = () => {
+  const userData = {
+    firstName: 'James',
+    lastName: 'Bond',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London',
+  };
+
   return (
-    <div>
-      <Profile
-        firstName={userData.firstName}
-        lastName={userData.lastName}
-        birthDate={userData.birthDate}
-        birthPlace={userData.birthPlace}
-      />
-    </div>
+    <Profile
+      userData={userData}
+      firstName={userData.firstName}
+      lastName={userData.lastName}
+      birthDate={userData.birthDate}
+      birthPlace={userData.birthPlace}
+    />
   );
 };
 

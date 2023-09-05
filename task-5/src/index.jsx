aee159d82e7aab53aa73d44de6d7f19a8366855b
Yuliia <106472993+Kozlovska-Yulya@users.noreply.github.com>
2023-09-05 11:24:37 +0300

    Delete task-5 directory

--- task-5/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.scss';
-import './profile.scss';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<App />, rootElement);
